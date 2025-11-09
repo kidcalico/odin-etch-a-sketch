ac10286dfea34abd04e65e49615166a78afc1a16
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2025-11-08 17:08:28 -0800

    Create the screen and fill with pixels

---
+++ script.js
@@ -0,0 +1,28 @@
+// Establish the container as a variable
+
+const container = document.querySelector("#container");
+
+// Create screen element within the container
+
+const screen = document.createElement("div");
+screen.style.cssText = "width: 540px; height: 540px; background-color: lightgray; margin: 0 auto; padding: 0; display: flex; flex-wrap: wrap;";
+container.appendChild(screen);
+
+// Create variables to calculate the size of pixels based on the
+// desired dimensions (size) and screen size
+
+const size = 16;
+const canvasSize = screen.offsetWidth;
+const pixelSize = (canvasSize/size);
+
+// Fill the screen with pixels
+
+for(i = 0; i < (size*size); i++) {
+    const pixel = document.createElement("div");
+    // pixel.style.cssText = "box-sizing: border-box; background-color: gray; flex: 1; border: 0.1px solid black;";
+    pixel.style.boxSizing = "border-box";
+    pixel.style.width = pixelSize + "px";
+    pixel.style.backgroundColor = "black";
+    pixel.style.border = "0.1px solid white";
+    screen.appendChild(pixel);
+}
