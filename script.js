11fe57af1a765b9f47dfe783465f2d3a8bee70ad
Kid Calico <43305187+kidcalico@users.noreply.github.com>
2025-11-10 19:01:44 -0800

    Add slider and border

--- script.js
+++ script.js
@@ -2,38 +2,50 @@
 
 const container = document.querySelector("#container");
 
+// Create a border around the screen element
+
+const border = document.createElement("div");
+border.style.cssText = "width: 640px; height: 640px; background-color: salmon; border-radius: 10px; margin: auto; align-content: center;";
+container.appendChild(border);
+
 // Create screen element within the container
 
 const screen = document.createElement("div");
-screen.style.cssText = "width: 540px; height: 540px; background-color: lightgray; margin: 0 auto; padding: 0; display: flex; flex-wrap: wrap;";
-container.appendChild(screen);
+screen.style.cssText = "width: 540px; height: 540px; background-color: lightgray; margin: auto; padding: 0; display: flex; flex-wrap: wrap;";
+border.appendChild(screen);
 
 // Create variables to calculate the size of pixels based on the
 // desired dimensions (size) and screen size
 
-const size = 16;
-const canvasSize = screen.offsetWidth;
-const pixelSize = (canvasSize/size);
+const myRange = document.querySelector("#myRange");
+
+
+myRange.addEventListener('change', screenSize);
 
 // Fill the screen with pixels
-
-for(i = 0; i < (size*size); i++) {
-    const pixel = document.createElement("div");
-    // pixel.style.cssText = "box-sizing: border-box; background-color: gray; flex: 1; border: 0.1px solid black;";
-    pixel.style.boxSizing = "border-box";
-    pixel.style.width = pixelSize + "px";
-    pixel.style.backgroundColor = "black";
-    // pixel.style.border = "0.1px solid white";
-    pixel.classList.add("pixel");
-    screen.appendChild(pixel);
-}
-
-// Change pixel color when the mouse enters a pixel
-
-const pixels = document.querySelectorAll(".pixel");
-
-pixels.forEach((pixel) => {
-    pixel.addEventListener("mouseenter", (event) => {
-        event.target.style.backgroundColor = "white";
+function screenSize () {
+    const size = myRange.value;
+    const canvasSize = screen.offsetWidth;
+    const pixelSize = (canvasSize/size);
+    console.log(myRange.value);
+    console.log(size);
+    for(i = 0; i < (size*size); i++) {
+        const pixel = document.createElement("div");
+        // pixel.style.cssText = "box-sizing: border-box; background-color: gray; flex: 1; border: 0.1px solid black;";
+        pixel.style.boxSizing = "border-box";
+        pixel.style.width = pixelSize + "px";
+        pixel.style.backgroundColor = "lightgray";
+        // pixel.style.border = "0.1px solid white";
+        pixel.classList.add("pixel");
+        screen.appendChild(pixel);
+    }
+    // Change pixel color when the mouse enters a pixel
+    
+    const pixels = document.querySelectorAll(".pixel");
+    
+    pixels.forEach((pixel) => {
+        pixel.addEventListener("mouseenter", (event) => {
+            event.target.style.backgroundColor = "black";
+        });
     });
-});
+}
